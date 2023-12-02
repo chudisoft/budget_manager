f9799ddb5e020eebd2f15eb2050b30711cb238fe
Christopher Uduekwe <christopheruduekwe@gmail.com>
2023-12-02 18:16:45 +0100

    update design

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  root 'home#index'
   devise_for :users
   # resources :groups #, only: [:new, :edit, :create, :destroy]
   # resources :entities #, only: [:new, :edit, :create, :destroy]
   resources :groups do
     resources :entities
   end
-  root 'groups#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
